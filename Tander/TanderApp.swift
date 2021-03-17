4fea9fbacb69470da4ed62051b641d15be856dc4
Ufuk Canlı <iletisim@ufukcanli.com>
2021-03-17 13:54:24 +0300

    Initial Commit

---
+++ Tander/TanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TanderApp.swift
+//  Tander
+//
+//  Created by Ufuk Canlı on 17.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
